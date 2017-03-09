54a992304e9959b9011a225c3b3ee5652810b7a1
yinghao-liu <yinghao_liu@163.com>
2017-03-09 13:44:10 -0800

    three chances to input password

--- connect.py
+++ connect.py
@@ -4,19 +4,35 @@
 import sys
 import getpass
 
-str=input("Are you sure you want to continue connecting (yes/no)?")
-while True:
-	if str == "yes":
-		break
-	elif str == "no":
-		print("Host key verification failed.")	
-		sys.exit()
-	else:
-		str=input("Please type 'yes' or 'no':")
+def main():
+	str=input("Are you sure you want to continue connecting (yes/no)?")
+	while True:
+		if str == "yes":
+			break
+		elif str == "no":
+			print("Host key verification failed.")	
+			sys.exit()
+		else:
+			str=input("Please type 'yes' or 'no':")
+	
+	
+	for i in range(3):
+		str=getpass.getpass("root@x.x.x.x's password:")
+		if str == "123456":
+			break
+		else:
+			if i != 2:
+				print("Permission denied, please try again.")
+			else:
+				print("Connection closed by x.x.x.x")
+				sys.exit()
 
-str=getpass.getpass("root@x.x.x.x's password:")
-if str != "123456":
-	print("Connection closed by x.x.x.x")
-print(str)
+	print(str)
 		
 
+
+#do		
+if __name__ == "__main__":
+	main()
+else:
+	pass
