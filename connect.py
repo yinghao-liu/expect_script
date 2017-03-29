f8e39ff084f481292946615e40d5c284e6b7ed31
yinghao-liu <yinghao_liu@163.com>
2017-03-29 12:11:24 -0700

    up to password

--- connect.py
+++ connect.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
-# simulate ssh process
-str=input("Are you sure you want to continue connecting (yes/no)?")
-print(str)
 
+# simulate ssh process
+import sys
+import getpass
+
+str=input("Are you sure you want to continue connecting (yes/no)?")
+while True:
+	if str == "yes":
+		break
+	elif str == "no":
+		print("Host key verification failed.")	
+		sys.exit()
+	else:
+		str=input("Please type 'yes' or 'no':")
+
+str=getpass.getpass("root@x.x.x.x's password:")
+if str != "123456":
+	print("Connection closed by x.x.x.x")
+print(str)
+		
 
